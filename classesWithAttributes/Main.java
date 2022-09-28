04b9587abc7a69a0e3567a7fca99da99852a44e1
orhancan96 <113685640+orhancan96@users.noreply.github.com>
2022-09-28 19:38:58 +0300

    Add files via upload

---
+++ classesWithAttributes/Main.java
@@ -0,0 +1,32 @@
+package classesWithAttributes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	
+		
+		
+		Product product= new Product (1,"Laptop","Asus",3000,2,"Siyah");
+		product.setName ("Laptop");
+		product.setId(1);
+	    product.setDescription("HP Laptop");
+		product.setPrice(4800);
+		product.setStockAmount(3);
+		product.setRenk("Siyah");
+		
+		
+		ProductManager productManager = new ProductManager();
+		productManager.Add(product);
+		
+		System.out.println(product.getKod());
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
